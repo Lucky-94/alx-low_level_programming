02b7517198c098b7b410e3e0940b546aa92fff31
Paul Ojukwu <paulojukwu98@gmail.com>
2022-12-09 15:14:25 -0800

    2-print_alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+*main - entry point
+*Return: Always 0
+*/
+
+int main(void)
+{
+	char ch;
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		putchar(ch);
+	}
+	return (0);
+}
