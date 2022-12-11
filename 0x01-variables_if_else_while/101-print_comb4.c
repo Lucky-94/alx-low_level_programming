3725a191b69db26c465d73db88cc785ed361ea88
Paul Ojukwu <paulojukwu98@gmail.com>
2022-12-11 01:15:31 -0800

    101-print_comb4

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+/**
+ * main - prints all possible combinations of three digit numbers,
+ * followed by a new line, using putchar
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int a;
+	int b;
+	int c;
+
+	for (a = 0; a <= 9; a++)
+	{
+		for (b = 1; b <= 9; b++)
+		{
+			for (c = 2; c <= 9; c++)
+			{
+				if (b == c || b > c)
+					continue;
+				if (a == b || a > b)
+					continue;
+				putchar(a);
+				putchar(b);
+				putchar(c);
+				if (a == 7 && b == 8 && c == 9)
+					continue;
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
