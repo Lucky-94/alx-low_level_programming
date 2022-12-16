c8e85dd8f0246239ac05997dfc8f21b8e38d7760
Paul Ojukwu <paulojukwu98@gmail.com>
2022-12-16 04:57:06 -0800

    3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints numbers from 0 to 9
+ * _putchar - prints the numbers to standard output
+ * Return: 0
+ */
+void print_numbers(void)
+{
+	char i;
+
+	for (i = '0'; i <= '9'; i++)
+		_putchar(i);
+}
