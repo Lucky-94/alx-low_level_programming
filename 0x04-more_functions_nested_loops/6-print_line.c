e55b6cc931b7145979d2caa6ecdc4d1d9bc2f0ce
Paul Ojukwu <paulojukwu98@gmail.com>
2022-12-16 08:45:58 -0800

    6-print_line.c

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -2,7 +2,7 @@
 
 /**
  * print_line - draws a straightline through the terminal
- * @n: number of times _ should be printed
+ * @n: number of times the character _ should be printed
  */
 void print_line(int n)
 {
