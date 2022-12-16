68f78a44f7b59540592ec7c539deff2de00398f1
Paul Ojukwu <paulojukwu98@gmail.com>
2022-12-15 20:53:39 -0800

    0-main.c

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,5 +1,4 @@
-lines (12 sloc)  190 Bytes
-#include"main.h"
+#include "main.h"
 
 /**
 * main - tests function that prints if integer is positive or negative
@@ -14,4 +13,4 @@ int main(void)
 	positive_or_negative(i);
 
 	return (0);
-}
+
