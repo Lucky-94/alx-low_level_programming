291558e35dd40a617292b80e0346d5bd2e4f1e78
Paul Ojukwu <paulojukwu98@gmail.com>
2023-01-06 09:33:24 -0800

    static library

---
+++ 0x09-static_libraries/100-atoi.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * _atoi-converts string to integer
+ * @s: string to be converted
+ * Return: an integer
+ */
+int _atoi(char *s)
+{
+	unsigned int num;
+	int sign;
+
+	num = 0;
+	sign = 1;
+	do {
+		if (*s == '-')
+		{
+			sign *= -1;
+		}
+		else if (*s >= '0' && *s <= '9')
+		{
+			num = num * 10 + (*s - '0');
+		}
+		else if (num > 0)
+		{
+			break;
+		}
+	} while (*s++);
+	num = sign * num;
+	return (num);
+}
