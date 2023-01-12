911842663392266076ff17565bb084ae0499b311
Paul Ojukwu <paulojukwu98@gmail.com>
2023-01-12 11:09:09 -0800

    1-string_nconcat.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -14,9 +14,9 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	unsigned int l1 = 0, l2 = 0, mem, i;
 
 	if (s1 == NULL)
-		*s1 = '\0';
+		s1 = "";
 	if (s2 == NULL)
-		*s2 = '\0';
+		s2 = "";
 	for (i = 0; s1[i]; i++)
 	{
 		l1 += 1;
