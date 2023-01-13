65f4b128b465f5c51d5d13ef8387ede1fa9f05a4
Paul Ojukwu <paulojukwu98@gmail.com>
2023-01-13 04:27:24 -0800

    100-realloc.c

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -30,13 +30,17 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 		return (ptr);
 	new_ptr = malloc(new_size);
 	if (new_ptr == NULL)
+	{
+		free(ptr);
 		return (NULL);
+	}
 	if (new_size >= old_size)
 	{
 		for (i = 0; i < old_size; i++)
 		{
 			new_ptr[i] = temp_ptr[i];
 		}
+		free(ptr);
 	}
 	return (new_ptr);
 }
