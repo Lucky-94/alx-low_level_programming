7cd9f36ae17107efb2dfd926659022146035efd5
Paul Ojukwu <paulojukwu99@gmail.com>
2023-01-19 06:15:44 -0800

    0-sum_them_all.c

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,24 @@
+#include "variadic_functions.h"
+#include <stdio.h>
+#include <stdarg.h>
+
+/**
+ * print_numbers-prints numbers followed by a new line
+ * @separator: separator string
+ * @n: number of expected integers
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	va_list nums;
+	unsigned int i;
+
+	va_start(nums, n);
+	for (i = 0; i < n; i++)
+	{
+		if (separator == NULL)
+			printf("%d", va_arg(nums, int));
+		else
+			printf("%d%s", va_arg(nums, int), separator);
+	}
+	printf("\n");
+}
