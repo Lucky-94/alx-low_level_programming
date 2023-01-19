682474c40cf9de5c5ccc6742b785590c74f9aef5
Paul Ojukwu <paulojukwu99@gmail.com>
2023-01-18 16:40:12 -0800

    3-main.c

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -10,27 +10,27 @@
 int main(int argc, char *argv[])
 {
 	int num1, num2, result;
-	char *operator;
+	char *opt;
 
 	if (argc != 4)
 	{
 		printf("Error\n");
 		exit(98);
 	}
-	operator = argv[2];
-	if (*operator != '+' && *operator != '-' && *operator != '*' && *operator != '/' && *operator != '%')
+	opt = argv[2];
+	if (*opt != '+' && *opt != '-' && *opt != '*' && *opt != '/' && *opt != '%')
 	{
 		printf("Error\n");
 		exit(99);
 	}
-	if ((*argv[2] == '/' && *argv[3] == '0') || (*argv[2] == '%' && *argv[3] == '0'))
+	if ((*opt == '/' && *argv[3] == '0') || (*opt == '%' && *argv[3] == '0'))
 	{
 		printf("Error\n");
 		exit(100);
 	}
 	num1 = atoi(argv[1]);
 	num2 = atoi(argv[3]);
-	result = (get_op_func(operator))(num1, num2);
+	result = (get_op_func(opt))(num1, num2);
 	printf("%d\n", result);
 	return (0);
 }
